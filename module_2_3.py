43ba6b82fabd45dc03a619757a1a9d0be234b31d
Alex-Hitrov <hitrov1973aa@gmail.com>
2024-12-02 13:01:58 +0300

    Update module_2_3.py
    Внесены изменения относительно выведения 0

--- module_2_3.py
+++ module_2_3.py
@@ -3,6 +3,9 @@ i = 0
 while i < len(my_list):
     if my_list[i] < 0:
         break
+    if my_list[i] == 0:
+        i += 1
+        continue
     else:
         print (my_list[i])
         i += 1
